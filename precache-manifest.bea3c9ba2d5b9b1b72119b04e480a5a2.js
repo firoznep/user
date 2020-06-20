f42cb033b9d6526a0b956009531d133125e2b219
firoznep <firoz.thekhan@gmail.com>
2020-06-21 02:25:35 +0545

    Updates

---
+++ precache-manifest.bea3c9ba2d5b9b1b72119b04e480a5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8924e164d99c8313fc45fe15aa257082",
+    "url": "/user/index.html"
+  },
+  {
+    "revision": "fe3d6a2b3279b8412033",
+    "url": "/user/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe3d6a2b3279b8412033",
+    "url": "/user/static/js/2.dfa77fda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user/static/js/2.dfa77fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4994ded1e6a42860003e",
+    "url": "/user/static/js/main.816089e2.chunk.js"
+  },
+  {
+    "revision": "430c172b6bd4ea03c75f",
+    "url": "/user/static/js/runtime-main.2e2439db.js"
+  }
+]);
