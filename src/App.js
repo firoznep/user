ddd35f658a958bfb6b37ef2c94d1a5c38eaba666
firoznep <firoz.thekhan@gmail.com>
2020-06-21 02:02:12 +0545

    app component modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h3>App</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-          commodi quasi eum, minus repudiandae praesentium? Qui et a vitae
-          cumque, harum rerum pariatur totam odit labore, praesentium minus? A,
-          consequuntur.
-        </p>
+      <div className="container">
+        <div className="card m-2 ">
+          <h3 className="card-title bg-primary text-white p-2">App</h3>
+          <div className="list-group m-2">
+            <div className="list-group-item">
+              <strong>Email: </strong>
+              <span>test.r@gmail.com</span>
+            </div>
+            <div className="list-group-item">
+              <strong>Phone: </strong>
+              <span>333-333-333</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
