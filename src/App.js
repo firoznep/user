141bc10f7d948e4c65028b68c581e05a12bf8c0c
firoznep <firoz.thekhan@gmail.com>
2020-06-21 01:25:22 +0545

    added paragraph to App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ class App extends React.Component {
     return (
       <div>
         <h3>App</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          commodi quasi eum, minus repudiandae praesentium? Qui et a vitae
+          cumque, harum rerum pariatur totam odit labore, praesentium minus? A,
+          consequuntur.
+        </p>
       </div>
     );
   }
